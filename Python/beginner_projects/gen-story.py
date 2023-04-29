bdb524d0afba3d628a6671ad6b11c40898a27db0
bloecher <bloechrr@gmail.com>
2023-04-29 19:38:42 +0200

    add gen-story

---
+++ Python/beginner_projects/gen-story.py
@@ -0,0 +1,7 @@
+navneord1 = input("Angiv et navneord i ental")
+
+navneordfler1 = input("Angiv et navneord i flertal")
+
+print("----------Historie Tid---------------")
+print("Der var engang en " + navneord1)
+print("han havde mange " + navneordfler1)
