8f5c32ffc398f5e95db4f33994634a26eacf8db7
bloecher <bloechrr@gmail.com>
2023-05-02 11:32:11 +0200

    Only AOC22

--- Python/beginner_projects/gen-story.py
+++
@@ -1,7 +0,0 @@
-navneord1 = input("Angiv et navneord i ental")
-
-navneordfler1 = input("Angiv et navneord i flertal")
-
-print("----------Historie Tid---------------")
-print("Der var engang en " + navneord1)
-print("han havde mange " + navneordfler1)
