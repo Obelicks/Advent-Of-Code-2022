2b79e72b52a963daa59331eddd3fc52fd4200a87
bloecher <bloechrr@gmail.com>
2023-05-03 14:41:37 +0200

    Solved 3rd of advent part 2

--- 3.py
+++ 3.py
@@ -14,7 +14,7 @@ def priority(item):
 
 
 
-def main():
+def main_part1():
     prioritysum = 0
 
     with open("data/3.txt") as file:
@@ -32,9 +32,31 @@ def main():
                 #print(p)
                 prioritysum += p
     
-    print(prioritysum)
+    return(prioritysum)
 
-main()
+def main_part2():
+    prioritysum = 0
+    with open("data/3.txt") as file:
+        data = [i for i in file.read().split("\n")]
+    if(DEBUG):
+        print(data)
+    while (len(data) > 0):
+        one = data.pop(0)
+        one =  [*set(one)]
+        two =data.pop(0)
+        three = data.pop(0)
+        if(DEBUG):
+            print(one ,"\n",two,"\n",three)
+        for i in one:
+            if(i in two and i in three):
+                p = priority(i)
+                if(DEBUG):
+                    print(i,": ",p)
+                prioritysum += p
+    return prioritysum    
+
+print(main_part1())
+print(main_part2())
 
 
 
